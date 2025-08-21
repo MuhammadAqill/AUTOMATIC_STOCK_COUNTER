66d3382c0e866d626581b851c1dfdea0ed4a0a9d
MuhammadAqill <maqil5697@gmail.com>
2025-08-21 23:58:44 +0800

    modified some file to run

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "/home/akira/esp/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "/home/akira/esp/esp-idf/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader"
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix"
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/tmp"
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/src"
+  "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/akira/AUTOMATIC_STOCK_COUNTER/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/akira/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
